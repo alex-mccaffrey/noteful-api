7a0055ce1eb8b27671d06da19b1847d62f4fc9ca
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-13 07:48:15 -0700

    basic structure in place, partial folder router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const notesRouter = require('./notes/notes-router')
+const foldersRouter = require('./folders/folders-router')
+
 
 const app = express()
 
@@ -15,6 +18,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use('/api/folders', foldersRouter)
+app.use('/api/notes', notesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
