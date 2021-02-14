2d2cd47417665743f1a5bf130fc6b23b4346435c
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-14 10:58:40 -0700

    endpoints

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -40,3 +40,57 @@ foldersRouter
         })
         .catch(next);
     });
+
+    foldersRouter
+    .route('/:folder_id')
+    .all((req, res, next) => {
+        FoldersService.getById(req.app.get('db'), req.params.folder_id)
+        .then((folder) => {
+        if (!folder) {
+            return res.status(404).json({
+                error: { message: `Folder doesn't exists` },
+            });
+        }
+        res.folder = folder; 
+        next();
+    })
+    .catch(next);
+    })
+.get((req, res, next) => {
+    res.json({
+        id: res.folder.id,
+        name: xss(res.folder.name),
+    });
+})
+.delete((req, res, next) => {
+    FoldersService.deleteFolder(req.app.get('db'),
+    req.params.folder_id)
+    .then(() => {
+        res.status(204).end();
+    })
+    .catch(next);
+})
+.patch(jsonParser, (req, res, next) => {
+    const { name } = req.body
+    const folderToUpdate = { name }
+    const numberOfValues = Object.values
+    (folderToUpdate).filter(Boolean).length
+    if (numberOfValues === 0) {
+        return res.status(400).json({
+            error: {
+                message: `Request body must contain 'name'`
+            }
+        })
+    }
+    FoldersService.updateFolder(
+        req.app.get('db'),
+        req.params.folder_id,
+        folderToUpdate
+    )
+    .then(numRowsAffected => {
+        res.status(204).end()
+    })
+    .catch(next)
+})
+
+module.exports = foldersRouter;
