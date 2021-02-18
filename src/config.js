be53b41c017867d7d467a526175445c9508ce437
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-18 08:05:12 -0700

    changes DATABASE reference

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DB_URL: process.env.DB_URL || 'postgresql://alex-mcc@localhost/noteful',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://alex-mcc@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://alex-mcc@localhost/noteful-test'
 }
