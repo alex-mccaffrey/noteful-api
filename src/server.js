be0a6b036634d8c43f6d47896a49431a27cfb960
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-19 20:30:49 -0700

    added ssl to db knex in server

--- src/server.js
+++ src/server.js
@@ -2,21 +2,13 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-});
-
-const db = knex({client})
-
-/*const db = knex({
-  client: 'pg',
-  connection: DATABASE_URL,
-})*/
+})
 
 app.set('db', db)
 
