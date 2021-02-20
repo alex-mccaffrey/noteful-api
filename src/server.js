ffefa58e2cfc4041b7db3acbefcff391699d5d7a
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-19 19:14:14 -0700

    removed ssl default in server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
+/*pg.defaults.ssl =
+  process.env.NODE_ENV === "production" ? {rejectUnauthorized: false}: false;*/
+
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: true,
 })
 
 app.set('db', db)
